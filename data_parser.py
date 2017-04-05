48e9b71f84eff80da5a33a0c5762e001e10f3bee
rogersjeffreyl <rl@cs.wisc.edu>
2017-04-04 20:39:10 -0500

    data parser

---
+++ data_parser.py
@@ -0,0 +1,36 @@
+from PIL import Image
+from io import BytesIO
+import pandas as pd
+import argparse
+import os
+import base64
+from scipy.misc import imread, imsave, imresize
+def generate_train_data(args):
+    for arg in args:
+        print (arg)
+        data_file = pd.read_csv(arg, header= None)
+        data_file.columns =["center_cam_image","left_cam_image","right_cam_image",\
+                            "angle","forward_throttle","reverse_throttle","speed"]
+        center_images =   data_file["center_cam_image"].values
+        for image_file in center_images:                 
+           
+           #image = Image.open(BytesIO(base64.b64decode(image_file)))
+           #image_array = np.asarray(image)
+           image_array = imread(image_file)
+           yield image_array
+
+        
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Training Data')
+    parser.add_argument(
+        '-train_data',
+        type=str,
+        nargs='+',
+        help='Path to the train data'
+    )
+    args = parser.parse_args()
+    print (args) 
+    generate_train_data(args.train_data)
+
+
+	
