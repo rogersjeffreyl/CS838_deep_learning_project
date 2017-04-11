614e2d1ee039c0850290cc56b0006e0e89c4eaa6
Rishab Kalra <rishabkalra009@gmail.com>
2017-04-11 18:50:09 -0500

    added support for reading multiple data files from folder and saving multiple models

--- cnn.py
+++ cnn.py
@@ -7,65 +7,65 @@ from keras import regularizers
 import data_parser as dp
 import argparse
 import numpy
+import glob
+import os
+from os.path import dirname
+import fnmatch
+
 def create_model(input_shape):
-	model = Sequential()
+    model = Sequential()
 
-	model.add(Conv2D(24, kernel_size=(5, 5), strides=(2, 2), activation='elu', input_shape=input_shape))
-	model.add(Conv2D(36, kernel_size=(5, 5), strides=(2, 2), activation='elu'))
-	model.add(Conv2D(48, kernel_size=(5, 5), strides=(2, 2), activation='elu'))
-	
-	model.add(Conv2D(64, kernel_size=(3, 3), activation='elu'))
-	model.add(Conv2D(64, kernel_size=(3, 3), activation='elu'))
+    model.add(Conv2D(24, kernel_size=(5, 5), strides=(2, 2), activation='elu', input_shape=input_shape))
+    model.add(Conv2D(36, kernel_size=(5, 5), strides=(2, 2), activation='elu'))
+    model.add(Conv2D(48, kernel_size=(5, 5), strides=(2, 2), activation='elu'))
 
-	model.add(Flatten())
+    model.add(Conv2D(64, kernel_size=(3, 3), activation='elu'))
+    model.add(Conv2D(64, kernel_size=(3, 3), activation='elu'))
 
-	model.add(Dense(100, activation='elu'))
-	model.add(Dense(50, activation='elu'))
-	model.add(Dense(10, activation='elu'))
-	model.add(Dense(1, activation='elu'))
+    model.add(Flatten())
 
-	model.compile(loss=keras.losses.mean_squared_error,
+    model.add(Dense(100, activation='elu'))
+    model.add(Dense(50, activation='elu'))
+    model.add(Dense(10, activation='elu'))
+    model.add(Dense(1, activation='elu'))
+
+    model.compile(loss=keras.losses.mean_squared_error,
               optimizer=keras.optimizers.Adadelta(),
               metrics=['accuracy'])
 
-	return model
+    return model
 
 def create_training_array(train_data):
-	x_train_arr = []
-	y_train_arr = []
+    x_train_arr = []
+    y_train_arr = []
+    count = 0
+    for value in dp.parse_data(train_data):
+        x_train_arr.append(value[0])
+        y_train_arr.append(value[1])
+        count+=1
 
-	count = 0
-	for value in dp.parse_data(train_data):
-		x_train_arr.append(value[0])
-		y_train_arr.append(value[1])
-		count+=1
-		if count == 1000:
-			break 
+    x_train = numpy.array(x_train_arr)
+    y_train = numpy.array(y_train_arr)
 
-	x_train = numpy.array(x_train_arr)
-	y_train = numpy.array(y_train_arr)
-
-	return (x_train, y_train)
+    return (x_train, y_train)
 
 def main():
-    parser = argparse.ArgumentParser(description='Training Data')
+    parser = argparse.ArgumentParser(description='Training Data Folder')
     parser.add_argument(
-        '-train_data',
+        '-train_data_folder',
         type=str,
-        nargs='+',
-        help='Path to the train data'
+        help='Path to the train data folder'
     )
+
     args = parser.parse_args()
+    
+    for root, dirnames, filenames in os.walk(args.train_data_folder):
+        for filename in fnmatch.filter(filenames, '*.csv'):
+            file = os.path.join(root, filename)
+            (x_train, y_train) = create_training_array(file)
 
-    (x_train, y_train) = create_training_array(args.train_data)
+            model = create_model(x_train[0].shape)
 
-    # if not x_train:
-    # 	print("No data")
-    # 	return
-
-    model = create_model(x_train[0].shape)
-
-    model.fit(x_train, y_train, batch_size=64, epochs = 12, verbose = 1)
-    model.save("cnn.h5")
+            model.fit(x_train, y_train, batch_size=64, epochs = 100, verbose = 1)
+            model.save(dirname(file)+"/cnn.h5")
 main()
-
--- data_parser.py
+++ data_parser.py
@@ -5,25 +5,25 @@ import os
 import base64
 from scipy.misc import imread, imsave, imresize
 def generate_train_data(args):
-    for arg in args:
-        print (arg)
-        data_file = pd.read_csv(arg, header= None)
-        data_file.columns =["center_cam_image","left_cam_image","right_cam_image",\
-                            "angle","forward_throttle","reverse_throttle","speed"]
-        center_images =   data_file["center_cam_image"].values
-        steering_angle =  data_file["angle"].values
-        
-        for index,image_file in enumerate(center_images):                 
-           
-           #image = Image.open(BytesIO(base64.b64decode(image_file)))
-           #image_array = np.asarray(image)
-           image_array = imread(image_file)
-           yield (image_array,steering_angle[index])
+  arg=args
+  print (arg)
+  data_file = pd.read_csv(arg, header= None)
+  data_file.columns =["center_cam_image","left_cam_image","right_cam_image",\
+                      "angle","forward_throttle","reverse_throttle","speed"]
+  center_images =   data_file["center_cam_image"].values
+  steering_angle =  data_file["angle"].values
+
+  for index,image_file in enumerate(center_images):                 
+     
+     #image = Image.open(BytesIO(base64.b64decode(image_file)))
+     #image_array = np.asarray(image)
+     image_array = imread(image_file)
+     yield (image_array,steering_angle[index])
            
         
 def parse_data(args):
     
-    return generate_train_data(args)
+  return generate_train_data(args)
 
 
 	
