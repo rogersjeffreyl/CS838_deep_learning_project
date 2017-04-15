37a96a292399b7afd6779b713bbb4e6da2ca8a86
rogersjeffreyl <rl@cs.wisc.edu>
2017-04-15 00:35:51 -0500

    data parser changes

--- data_parser.py
+++ data_parser.py
@@ -4,11 +4,13 @@ import pandas as pd
 import os
 import base64
 import numpy
+
 from scipy.misc import imread, imsave, imresize
 def generate_train_data(args):
   arg=args
   print (arg)
   data_file = pd.read_csv(arg, header= None)
+  csv_folder, csv_file = os.path.split(arg)
   data_file.columns =["center_cam_image","left_cam_image","right_cam_image",\
                       "angle","forward_throttle","reverse_throttle","speed"]
   center_images =   data_file["center_cam_image"].values
@@ -18,6 +20,8 @@ def generate_train_data(args):
      
      #image = Image.open(BytesIO(base64.b64decode(image_file)))
      #image_array = np.asarray(image)
+     image_folder, image_file = os.path.split(image_file)
+     image_file =os.path.join(csv_folder,"IMG",image_file)
      image = Image.open(image_file)
      width, height = image.size
      image = image.crop((0,77 , width, height))
